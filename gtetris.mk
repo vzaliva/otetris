6e6aaad520da0d38c53c01767b044d898906d785
Vadim Zaliva <lord@crocodile.org>
2015-03-23 22:12:10 -0700

    font from http://www.aaroncox.net/tutorials/2dtutorials/ARIAL.TTF

--- gtetris.mk
+++ gtetris.mk
@@ -1,7 +1,7 @@
 PACKS = batteries sdl.sdlimage sdl.sdlmixer sdl.sdlttf sdl
 RESULT     = gtetris
 SOURCES    = tetris.ml gtetris.ml
-LIBS       = Bigarray batteries sdl
+LIBS       = batteries sdl
 INCDIRS    = $(HOME)/.opam/system/lib/sdl
 OCAMLLDFLAGS = -cclib "-framework Cocoa"
 THREADS = true

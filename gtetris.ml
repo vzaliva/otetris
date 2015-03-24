9799259c0064bd4d4c6ce33e58f84168d06a4c16
Vadim Zaliva <lord@crocodile.org>
2015-03-24 11:45:33 -0700

    experimenting with event handling

--- gtetris.ml
+++ gtetris.ml
@@ -11,6 +11,8 @@ let board_width = 10 and board_height = 22
 (* Using fixed gravity  for now *)
 let gravity = 0.05
 
+let tickUserEventNo = 0
+                
 let rec loop state =
   let cstate = !state in
   let rstate = initial_state cstate.width cstate.height in
@@ -30,11 +32,24 @@ let rec loop state =
   | KEYDOWN {keysym=KEY_RIGHT} ->
      state := if cstate.over then rstate else update_state MoveRight cstate;
      loop state
+  | USER tickUserEventNo ->
+     print_string "Hello1\n";
+     (if not cstate.over then
+       state := update_state Tick cstate);
+     loop state
   | KEYDOWN {keysym=KEY_ESCAPE} ->
      ()
   | _ ->
      loop state
 
+let rec timer_fun (flag, callback) =
+  if !flag then
+    Thread.exit
+  else
+    (Thread.delay 1.;
+     callback;
+     timer_fun (flag, callback))
+                    
 let main () =
   Random.self_init();
   let (state:(Tetris.state ref)) = ref (initial_state board_width board_height) in
@@ -44,6 +59,11 @@ let main () =
   Sdlttf.init ();
   at_exit Sdlttf.quit;
   Sdlwm.set_caption ~title:"GTetris" ~icon:"GTetris";
-  loop state
+  let timer_flag = ref false in
+  let timer_cb = print_string "Hello0\n"; Sdlevent.add [USER tickUserEventNo] in
+  let timer_thread = Thread.create timer_fun (timer_flag, timer_cb) in
+  loop state;
+  timer_flag := true;
+  Thread.join timer_thread
 
 let _ = main ()                

0f19b81d94c463dea8cb7c8dcbe27babae1cf09b
Vadim Zaliva <lord@crocodile.org>
2015-04-01 22:49:32 -0700

    show legend

--- gtetris.ml
+++ gtetris.ml
@@ -24,8 +24,7 @@ let glass_color = (255,255,255)
 let tickUserEventNo = 0
 let opaque = 255 (* for alpha channel values in RGBA *)
 let font_filename  = "arial.ttf"
-let font_size = 24
-let info_area_width = 400
+let legend_area_width = 400
 
 (* font cache *)
 module IntMap = Map.Make(struct type t = int let compare : int -> int -> int = compare end)                        
@@ -81,7 +80,37 @@ let get_font (sz:int) : Sdlttf.font =
     font_cache := IntMap.add sz f c ;
     f
 
+
+let txt_at txt x y size color screen =
+  let font = get_font size in
+  let text = render_text_blended font txt ~fg:color in
+  let (t_w,t_h,_) = surface_dims text in
+  let text_box = rect x y t_w t_h in
+  blit_surface ~dst_rect:text_box ~src:text ~dst:screen ();
+  ()
+
+let draw_legend screen r =
+  let score_size = 20 in
+  txt_at "Score:" (r.r_x+score_size) r.r_y score_size cyan screen;
+  txt_at "Level:" (r.r_x+200) r.r_y score_size blue screen;
+  txt_at "Controls:" (r.r_x+30) (r.r_y+125) 20 white screen;
+  let help_size = 18 in
+  let  x = (r.r_x+50) and
+       x1 = (r.r_x+150) and
+       y = (r.r_y+150) in
+  txt_at "<-, ->" x y help_size yellow screen;
+  txt_at "move" x1 y help_size green screen;
+  txt_at "Up, Down" x (y+20) help_size yellow screen;
+  txt_at "rotate" x1 (y+20) help_size green screen;
+  txt_at "Space" x (y+40) help_size yellow screen;
+  txt_at "drop" x1 (y+40) help_size green screen;
+  txt_at "Esc" x (y+60) help_size yellow screen;
+  txt_at "quit" x1 (y+60) help_size green screen;
+  let url_size = 16 in
+  txt_at "https://github.com/vzaliva/otetris" (r.r_x+170) (r.r_y+r.r_h-2*url_size) url_size blue screen
+
 let show_game_over screen e =
+  let font_size = 24 in
   let font = get_font font_size in
   let text = render_text_blended font "Game Over" ~fg:Sdlvideo.white in
   let (t_w,t_h,_) = surface_dims text in
@@ -145,13 +174,14 @@ let main () =
   Random.self_init();
   let (state:(Tetris.state ref)) = ref (initial_state board_width board_height) in
   Sdl.init [`VIDEO];
-  let screen_width = block_side*(board_width+2)+info_area_width and screen_height=block_side*(board_height+1) in
-  let screen = set_video_mode screen_width screen_height [`DOUBLEBUF] in
+  let glass_width = block_side*(board_width+2) and glass_height=block_side*(board_height+1) in
+  let screen = set_video_mode (glass_width+legend_area_width) glass_height [`DOUBLEBUF] in
   at_exit Sdl.quit;
   Sdlttf.init ();
   at_exit Sdlttf.quit;
   Sdlwm.set_caption ~title:"GTetris" ~icon:"GTetris";
   draw_walls screen;
+  draw_legend screen (rect glass_width 0 legend_area_width glass_height);
   let timer_flag = ref false
   and timer_cb () = Sdlevent.add [USER tickUserEventNo]  in
   let timer_thread = Thread.create timer_loop (timer_flag, timer_cb) in
